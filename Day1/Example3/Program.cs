10679b7535e345a681bdcf6cd9b179003ca5b043
Sergey Komissarov <lourtne@gmail.com>
2022-11-22 23:40:48 +0300

    First commit

---
+++ Day1/Example3/Program.cs
@@ -0,0 +1,32 @@
+﻿/*Задача №3. Напишите программу, которая будет выдавать название дня недели по заданному номеру.
+3 -> Среда 
+5 -> Пятница
+*/
+
+Console.WriteLine("Введите день недели");
+string number1Str = Console.ReadLine();
+int number1 = Convert.ToInt32(number1Str);
+
+if(number1 == 1)
+{
+Console.WriteLine("пн");
+} else if (number1 == 2){
+    Console.WriteLine("вт");
+}
+
+else if (number1 == 3){
+    Console.WriteLine("ср");
+}
+else if (number1 == 4){
+    Console.WriteLine("чт");
+}
+else if (number1 == 5){
+    Console.WriteLine("пт");
+}
+else if (number1 == 6){
+    Console.WriteLine("сб");
+}
+else if (number1 == 7){
+    Console.WriteLine("вс");
+} else
+    Console.WriteLine("ввели не число недели (1-7)");
---
+++ Day1/Example5/Program.cs
@@ -0,0 +1,18 @@
+﻿/*Задача №7. Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает последнюю цифру этого числа.
+	456 -> 6
+	782 -> 2
+	918 -> 8
+*/
+
+Console.WriteLine("Введите трехзначное число");
+int number = Int32.Parse(Console.ReadLine());
+if (number < 1000 && number > 99)
+{ 
+    int result = number % 10;
+    Console.WriteLine(result); 
+}
+else
+{
+    Console.WriteLine("Wrong number");
+    return;
+}
