28304e1919b07b5b98c1b1361fa748dd9bad6d59
Sergey Komissarov <lourtne@gmail.com>
2022-12-03 20:44:02 +0300

    4 hw

---
+++ C# - education/Seminar_4/Homework/Task_27/Program.cs
@@ -0,0 +1,45 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+//получение числа с консоли
+int GetNumber(string message)
+{
+    bool isCorrect = false;
+    int result = 0;
+
+    while(!isCorrect)
+    {
+        Console.WriteLine(message);
+        if(int.TryParse(Console.ReadLine(), out result))
+        {
+            isCorrect = true;
+        }
+        else
+        {
+            Console.WriteLine("Ввели не число");
+        }
+    }
+    
+    return result;
+}
+
+  int SumNumber(int numberN){
+    
+    int counter = Convert.ToString(numberN).Length;
+    int advance = 0;
+    int result = 0;
+
+    for (int i = 0; i < counter; i++){
+      advance = numberN - numberN % 10;
+      result = result + (numberN - advance);
+      numberN = numberN / 10;
+    }
+   return result;
+  }
+
+int number = GetNumber("Введите число");
+int sumNumber = SumNumber(number);
+Console.WriteLine("Сумма цифр в числе: " + sumNumber);
