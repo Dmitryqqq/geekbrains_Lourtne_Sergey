7f474a6e84afd2d2727fc7d9c10ded7d7430312d
Sergey Komissarov <lourtne@gmail.com>
2022-12-05 20:37:25 +0300

    5 sem

---
+++ C# - education/Seminar_5/Task_1/Program.cs
@@ -0,0 +1,35 @@
+﻿//инициализирует новый массив
+int[] InitArray(int length)
+{
+    int[] array = new int[length];
+    Random rnd = new Random();
+    for (int i = 0; i < length; i++)
+    {
+        array[i] = rnd.Next(-9,10);
+    }
+
+    return array;
+}
+
+//печатает массив на экран
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+
+//получает сумму положительных элементов в массиве
+int GetPositiveSumm(int[] arr)
+{
+    int summ = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if(arr[i] > 0)
+            summ += arr[i];
+    }
+
+    return summ;
+}
