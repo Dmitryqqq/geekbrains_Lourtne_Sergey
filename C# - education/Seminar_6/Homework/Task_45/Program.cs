1963fe3d7ea6d1e2b2c94e5f057ca7fc6d2d0382
Sergey Komissarov <lourtne@gmail.com>
2022-12-14 20:59:40 +0300

    6 hw

---
+++ C# - education/Seminar_6/Homework/Task_45/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+public class Example
+{
+	public static void Main()
+	{
+		int[] arr = { 5, 4, 7, 2, 9 };
+		int start = 1;
+		int length = 3;
+
+		int[] copy = new int[length];
+		Array.Copy(arr, start, copy, 0, length);
+
+		Console.WriteLine(String.Join(", ", copy));
+	}
+}
