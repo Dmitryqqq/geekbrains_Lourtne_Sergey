42d362aa66965e6836a89b64f8c5392a38c49cc9
Sergey Komissarov <lourtne@gmail.com>
2023-03-07 18:18:46 +0300

    1tak

--- Java - education/Seminar2/hw/task1.java
+++ Java - education/Seminar2/hw/task1.java
@@ -27,18 +27,28 @@ public class task1 {
     public static String bubble(int[] mas) {
         boolean isSorted = false;
         int buf;
-        while(!isSorted) {
-            isSorted = true;
-            for (int i = 0; i < mas.length-1; i++) {
-                if(mas[i] > mas[i+1]){
-                    isSorted = false;
- 
-                    buf = mas[i];
-                    mas[i] = mas[i+1];
-                    mas[i+1] = buf;
+        String file_name = "Java - education/Seminar2/hw/log.txt";
+        File file = new File(file_name);
+        try{
+            FileWriter writer = new FileWriter(file,true);
+            while(!isSorted) {
+                isSorted = true;
+                for (int i = 0; i < mas.length-1; i++) {
+                    if(mas[i] > mas[i+1]){
+                        isSorted = false;
+    
+                        buf = mas[i];
+                        mas[i] = mas[i+1];
+                        mas[i+1] = buf;
+                    }
+                    writer.write(i);
+                    writer.write("\n");
                 }
             }
-        }
+            writer.close();
+    } catch (Exception e){ 
+        System.out.println("Что то пошло не так");
+    }
         return Arrays.toString(mas);
 }
 }
